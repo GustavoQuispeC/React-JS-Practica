123e0140cd67df4268f954a7127adacfef815830
Gustavo <gusstavocta@gmail.com>
2024-08-11 00:22:51 -0500

    agregando primer componente

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,11 +1,27 @@
-
+import { useState } from "react";
+import AddCategory from "./components/AddCategory";
 
 const GifApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+    const onAddCategory = () => {
+      const NewCategories = [...categories, "Valorant"];
+      setCategories(NewCategories);
+    };
+
   return (
     <>
-    <h1>GifAPP</h1>
-    </>
-  )
-}
+      <h1>GifAPP</h1>
 
-export default GifApp
+      <AddCategory setCategories={setCategories} />
+
+      <ol>
+        {categories.map((x) => {
+          return <li key={x}>{x}</li>;
+        })}
+      </ol>
+    </>
+  );
+};
+
+export default GifApp;
