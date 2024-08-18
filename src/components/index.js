3429965e232ded6ba9cc223ba5bf52cd674f2015
Gustavo <gusstavocta@gmail.com>
2024-08-17 23:57:09 -0500

    comentario

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as AddCategory } from "./AddCategory";
 export { default as GifGrid } from "./GifGrid";
-//xport { default as GifItem } from "./GifItem";
+export { default as GifItem } from "./GifItem";
